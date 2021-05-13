bdeea1b4df4000e072b02f9890da3285d8c5d055
Pedro Muñoz <munoz@users.noreply.github.com>
2021-05-13 12:58:59 -0700

    Integrated maps dependencies

--- app/src/main/java/com/cp_kotlin/FragmentMaps.kt
+++ app/src/main/java/com/cp_kotlin/FragmentMaps.kt
@@ -1,27 +1,29 @@
 package com.cp_kotlin
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import android.view.*
-import android.widget.Button
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import com.cp_kotlin.ui.main.SectionsPagerAdapter
-import com.google.android.material.transition.MaterialSharedAxis
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class FragmentMaps : Fragment() {
+
+class FragmentMaps : Fragment()  {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false)
-    }
+        val view: View = inflater.inflate(R.layout.fragment_maps, container, false);
 
+
+        // Inflate the layout for this fragment
+        return view;
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
